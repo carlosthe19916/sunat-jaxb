cc6c4e541f8a7e5ada07f92067a711ee3ce247fa
Carlos Feria <carlosthe19916@gmail.com>
2018-04-28 09:02:27 -0500

    test

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_77a57dd353f9_key -iv $encrypted_77a57dd353f9_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import cd/codesigning.asc
 fi
