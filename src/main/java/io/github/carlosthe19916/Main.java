c3b1f92837522ef1069404625e42fa045f3f84c1
Carlos Feria <carlosthe19916@gmail.com>
2018-04-28 11:13:02 -0500

    test

---
+++ src/main/java/io/github/carlosthe19916/Main.java
@@ -0,0 +1,10 @@
+package io.github.carlosthe19916;
+
+import oasis.names.specification.ubl.schema.xsd.invoice_2.InvoiceType;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InvoiceType invoiceType = new InvoiceType();
+    }
+}
